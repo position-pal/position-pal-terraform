8eeb4eca453b09bcedc77e989f5edc8b4e0f53d3
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2025-02-03 22:16:58 +0100

    feat: adding chat-service base files

---
+++ plan/chat_service.tf
@@ -0,0 +1,24 @@
+resource "helm_release" "chat_service" {
+  name       = "chat-service"
+  repository = "oci://ghcr.io/position-pal/"
+  chart      = "position-pal-chat-service"
+  namespace  = "pp-cs"
+  version = "1.0.2"
+
+  create_namespace = true
+
+  set {
+    name = "rabbitmq.username"
+    value = var.rabbitmq_username
+  }
+
+  set {
+    name = "rabbitmq.password"
+    value = var.rabbitmq_password
+  }
+  
+  set {
+    name = "akkaLicenseKey"
+    value = var.akka_license_key
+  }
+}
