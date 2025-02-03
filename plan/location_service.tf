b75880f79d797bc21ade7f6f2adec0094afaab3d
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2025-02-03 17:42:37 +0100

    feat: add base files for the location service

---
+++ plan/location_service.tf
@@ -0,0 +1,24 @@
+resource "helm_release" "location_service" {
+  name       = "location-service"
+  repository = "oci://ghcr.io/position-pal/"
+  chart      = "position-pal-location-service"
+  namespace  = "pp-ls"
+  version = "1.1.0"
+
+  create_namespace = true
+
+  set {
+    name = "rabbitmq.password"
+    value = var.rabbitmq_password
+  }
+
+  set {
+    name = "mapboxApi"
+    value = var.mapbox_key
+  }
+  
+  set {
+    name = "akkaLicenseKey"
+    value = var.akka_license_key
+  }
+}
---
+++ plan/location_service_variables.tf
@@ -0,0 +1,5 @@
+variable "mapbox_key" {
+  type = string
+  sensitive = true
+  description = "Map box API key"
+}
