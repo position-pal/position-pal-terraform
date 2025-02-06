5693a689323f259e9ddc9be9bd6f6c11571fd89e
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2025-02-06 19:18:07 +0100

    fix: create namespace clause set to true

--- plan/gateway_service.tf
+++ plan/gateway_service.tf
@@ -5,7 +5,8 @@ resource "helm_release" "gateway_service" {
   namespace  = "pp-gws"
   version = "1.0.0"
   
-  
+  create_namespace = true
+
   depends_on = [ 
     helm_release.chat_service,
     helm_release.location_service,
