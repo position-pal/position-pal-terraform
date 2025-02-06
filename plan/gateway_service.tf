385c66e823b4d3738107bffd9df9bc56d944b363
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2025-02-06 20:13:09 +0100

    feat: add configuration for the service

--- plan/gateway_service.tf
+++ plan/gateway_service.tf
@@ -6,13 +6,36 @@ resource "helm_release" "gateway_service" {
   version = "1.0.0"
   
   create_namespace = true
+  
+  set {
+    name = "config.chatServiceGrpcUrl"
+    value = "chat-service.pp-cs:5052"
+  }
+  set {
+    name = "config.chatServiceHttpUrl"
+    value = "chat-service.pp-cs:8080"
+  }
+  set {
+    name = "config.locationServiceHttpUrl"
+    value = "location-service.pp-ls:8080"
+  }
+  set {
+    name = "config.locationServiceGrpcUrl"
+    value = "location-service.pp-ls:5052"
+  }
+  set {
+    name = "config.userServiceGrpcUrl"
+    value = "user-service.pp-us:5052"
+  }
+  set {
+    name = "config.notificationServiceGrpcUrl"
+    value = "notification-service.pp-ns:5052"
+  }
 
   depends_on = [ 
     helm_release.chat_service,
     helm_release.location_service,
     helm_release.notification_service,
     helm_release.user-service
- ]
-
- 
+    ]
 }
