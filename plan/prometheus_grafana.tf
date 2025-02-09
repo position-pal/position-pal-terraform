e6fee20e811ac5aca2a19a36c353979d7810667d
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2025-02-09 18:41:51 +0100

    refactor: change release name

--- plan/prometheus_grafana.tf
+++ plan/prometheus_grafana.tf
@@ -1,5 +1,18 @@
 resource "helm_release" "prometheus_grafana" {
   name       = "prometheus-grafana"
   repository = "https://prometheus-community.github.io/helm-charts"
-  chart = ""
+  chart = "prometheus-community/kube-prometheus-stack"
+
+  namespace  = "monitoring"
+  version = "69.2.0"
+  
+  set {
+    name = "grafana.adminUser"
+    value = var.grafana_admin
+  }
+
+  set {
+    name = "grafana.adminPassword"
+    value = var.grafana_password
+  }
 }
